4348ac2bbaa2b3e8ad1a3fdbe84fe73050bc4b24
Suman <sumansinghrajput709@gmail.com>
2023-04-07 20:32:18 +0530

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(http_method_names=['GET', 'HEAD'])
+def ping(request):
+    print("hi")
+    return Response({
+        'message': 'successful',
+        'success': True,
+    })
